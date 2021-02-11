0a1bb18bae51528e6e876b0e12081caa88cfc1e2
luiz fernando <luiz123jfmg@gmail.com>
2021-02-11 17:09:22 -0300

    Criando operação de instanciação de categorias ao rodar projeto

--- src/main/java/com/br/curso/CursoSpringApplication.java
+++ src/main/java/com/br/curso/CursoSpringApplication.java
@@ -1,13 +1,34 @@
 package com.br.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.br.curso.domain.Categoria;
+import com.br.curso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoSpringApplication {
+public class CursoSpringApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria c1 = new Categoria(null,"Informática");
+		Categoria c2 = new Categoria(null,"Escritório");
+		Categoria c3 = new Categoria(null,"Marketing");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 
 }
