7a9be002cecb4030165d2749be7b03c0598e3de3
luiz fernando <luiz123jfmg@gmail.com>
2021-02-11 17:43:43 -0300

    Criado o ecosistema de produto juntamente com os relacionamentos com a tabela categoria

--- src/main/java/com/br/curso/CursoSpringApplication.java
+++ src/main/java/com/br/curso/CursoSpringApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.br.curso.domain.Categoria;
+import com.br.curso.domain.Produto;
 import com.br.curso.repositories.CategoriaRepository;
+import com.br.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner{
@@ -18,8 +20,12 @@ public class CursoSpringApplication implements CommandLineRunner{
 	}
 	
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -27,8 +33,21 @@ public class CursoSpringApplication implements CommandLineRunner{
 		Categoria c2 = new Categoria(null,"Escritório");
 		Categoria c3 = new Categoria(null,"Marketing");
 		
-		categoriaRepository.saveAll(Arrays.asList(c1,c2,c3));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 110.00);
 		
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().addAll(Arrays.asList(p3));
+
+
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2,c3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
