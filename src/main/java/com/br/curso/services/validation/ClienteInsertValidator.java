91034589525ae8ca63c62f5ac89a1dca4962999f
luiz fernando <luiz123jfmg@gmail.com>
2021-02-24 15:56:52 -0300

    Validando email já existente na inserção de cliente

--- src/main/java/com/br/curso/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/br/curso/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.br.curso.domain.Cliente;
 import com.br.curso.domain.enums.TipoCliente;
 import com.br.curso.dto.ClienteNewDTO;
+import com.br.curso.repositories.ClienteRepository;
 import com.br.curso.resources.exception.FieldMessage;
 import com.br.curso.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,6 +27,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
+		Cliente cli = repo.findByEmail(objDto.getEmail());
+		
+		if(cli != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
