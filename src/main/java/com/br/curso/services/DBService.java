4cd9557560917b2c86800bdf38320ce1f04aa005
luiz fernando <luiz123jfmg@gmail.com>
2021-03-01 11:26:29 -0300

    Ajuste perfil desenvolvimento

--- src/main/java/com/br/curso/services/DBService.java
+++ src/main/java/com/br/curso/services/DBService.java
@@ -68,7 +68,7 @@ public class DBService {
 		Categoria cat4 = new Categoria(null,"Cama mesa e banho");
 		Categoria cat5 = new Categoria(null,"Decoração");
 		Categoria cat6 = new Categoria(null,"Perfumaria");
-		
+		Categoria cat7 = new Categoria(null,"Roupas");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -82,7 +82,7 @@ public class DBService {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
