f5b26cacf976186e99db8e28c58a55dbab5174ba
luiz fernando <luiz123jfmg@gmail.com>
2021-03-09 11:40:13 -0300

    Configurando arquivos properties

--- src/main/java/com/br/curso/services/DBService.java
+++ src/main/java/com/br/curso/services/DBService.java
@@ -121,7 +121,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "12345678952", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "luiz123jfmg@gmail.com", "12345678952", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("32999956148", "3299654158"));
 		
 		
