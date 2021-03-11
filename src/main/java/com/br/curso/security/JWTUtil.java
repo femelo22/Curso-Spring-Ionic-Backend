796a36bfff4b490bd26c1e7a821c2b771184aa89
luiz fernando <luiz123jfmg@gmail.com>
2021-03-11 15:30:30 -0300

    Gerando nosso Token JWT

---
+++ src/main/java/com/br/curso/security/JWTUtil.java
@@ -0,0 +1,29 @@
+package com.br.curso.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+	
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	public String  generateToken(String username) {// gerar nosso TOKEN JWT
+		return Jwts.builder()
+				.setSubject(username)
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+	}
+	
+}
