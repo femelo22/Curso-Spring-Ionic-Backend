be4d7492613d42f67e94057c1e391e646704a14d
luiz fernando <luiz123jfmg@gmail.com>
2021-04-12 10:01:40 -0300

    Criando classes para upload de arquivo s3Service

--- src/main/java/com/br/curso/CursoSpringApplication.java
+++ src/main/java/com/br/curso/CursoSpringApplication.java
@@ -1,27 +1,18 @@
 package com.br.curso;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.br.curso.services.S3Service;
-
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
-		s3.uploadFile("C://img/cat1.jpg");
-		
+	public void run(String... args) throws Exception {		
 		System.out.println("R O D A N T E   ✔✔✔✔");
 	}
 
